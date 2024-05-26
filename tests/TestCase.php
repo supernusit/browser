39ee09631fbf3126ce8db98cf061076c49b750c0
supernusit <appdevteam@supernus.com>
2024-05-25 18:10:38 -0600

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Asciito\Browser\Tests;
 
-use Asciito\Browser\BrowserServiceProvider;
+use Browser\BrowserServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
